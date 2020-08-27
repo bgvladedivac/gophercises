38ce27ce560c08a9ba5804259deef43bcab815a9
bgvladedivac <bgvladedivac@gmail.com>
2020-08-27 20:20:56 +0300

    Cover all the requirements, left room for imrovements

--- 1.QuizGame/solve.go
+++ 1.QuizGame/solve.go
@@ -5,7 +5,10 @@ import (
 	"os"
 	"fmt"
 	"flag"
+	"time"
 )
+
+const Ready = "yes"
 
 func getFileContent(filename string) (records[][] string, err error) {
 	fd, err := os.Open(filename)
@@ -28,17 +31,40 @@ func exit(message string) {
 	os.Exit(3)
 }
 
+func inScopeWithTime(seconds int64) {
+	time.Sleep(time.Duration(seconds) * time.Second)
+	fmt.Println("Your time has ended.")
+	fmt.Println("You had", seconds, "seconds.")
+	os.Exit(1)
+}
+
+func promptUser(allowedTime int64) {
+	//if ready start out the goutine to check if time has been exceeded
+	fmt.Println("Type 'yes' if you are ready to start")
+	var ready string
+	fmt.Scanf("%s", &ready)
+
+	if ready == Ready {
+		go inScopeWithTime(allowedTime)
+	} else {
+		promptUser(allowedTime)
+	}
+}
+
 func main() {
-	ptrToFileName := flag.String("name of csv file", "problems.csv", "The name of the csv file - default to 'problems.csv'")
-	records, err := getFileContent(*ptrToFileName)
+	fileName := flag.String("name of csv file", "problems.csv", "The name of the csv file - default to 'problems.csv'")
+	allowedTime := flag.Int64("allowed time", 30, "The duration in seconds for which the quizz must be finished.")
+
+	records, err := getFileContent(*fileName)
 
 	if err != nil {
 		exit("Reader problem.")
 	}
 
 
-	counter := 0
+	promptUser(*allowedTime)
 
+	counter := 0
 	for _, value := range records {
 		question, answer  := value[0], value[1]
 		fmt.Println("Question:", question)
