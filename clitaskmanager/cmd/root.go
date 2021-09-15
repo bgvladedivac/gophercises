70daf82c7316083a46ee5361215a02bff6c567af
bgvladedivac <bgvladedivac@gmail.com>
2021-09-15 22:36:54 +0300

    Update cmd/root.go

--- clitaskmanager/cmd/root.go
+++ clitaskmanager/cmd/root.go
@@ -1,7 +1,10 @@
 package cmd
 
+import (
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
-  Use:   "task",
-  Short: "task is a cli for a todo app",
+	Use:   "task",
+	Short: "task is a cli for a todo app",
 }
