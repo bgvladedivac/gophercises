bcc9b4e4e4b2b3a4cec65a17687905f7aafbab7a
bgvladedivac <bgvladedivac@gmail.com>
2021-09-16 13:38:35 +0300

    Expose Execute

--- clitaskmanager/cmd/root.go
+++ clitaskmanager/cmd/root.go
@@ -1,10 +1,16 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "task",
 	Short: "task is a cli for a todo app",
+}
+
+func Execute() {
+	fmt.Println("Execute function was called.")
 }
